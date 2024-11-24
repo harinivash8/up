4bb366944a470cc337ef0a953d822912c89321a7
gavalian <gavalian@jlab.org>
2024-11-23 22:56:42 -0500

    first commit of the sources

---
+++ src/main/java/twig/demo/RunDemo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package twig.demo;
+
+import twig.graphics.TGCanvas;
+
+/**
+ *
+ * @author gavalian
+ */
+public class RunDemo {
+    public static void main(String[] args){
+        TGCanvas c = new TGCanvas("Demo Canvas",500,500);
+    }
+}
