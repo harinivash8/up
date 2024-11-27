87d4ed9b7b04b14b0a5b22bea7bcbee8818abdbf
Gagik Gavalian <gavalian@users.noreply.github.com>
2024-11-26 22:28:53 -0500

    updated the figures

--- tutorials/plotting/advanced_h1d_fitting.java
+++ tutorials/plotting/advanced_h1d_fitting.java
@@ -7,8 +7,8 @@
 import java.awt.Font;
 
 TStyle.getInstance().setDefaultPaveTextFont( new Font("Arial",Font.PLAIN,18));
-TStyle.getInstance().setDefaultAxisLabelFont(new Font("Arial",Font.PLAIN,18));
-TStyle.getInstance().setDefaultAxisTitleFont(new Font("Arial",Font.PLAIN,20));
+TStyle.getInstance().setDefaultAxisLabelFont(new Font("Arial",Font.PLAIN,20));
+TStyle.getInstance().setDefaultAxisTitleFont(new Font("Arial",Font.PLAIN,22));
 
 Random r = new Random();
 H1F h = new H1F("h100","random+gaus;x-axis;count",120,0.0,1.0);
@@ -18,7 +18,7 @@ for(int i = 0; i < 3600; i++){
     for(int j = 0; j < 20;j++) h.fill((r.nextDouble()+r.nextDouble())*2);
 }
 
-h.attr().set("mc=3,ms=8,lc=3,lw=2");
+h.attr().set("mc=3,ms=8,lc=3,lw=2,ms=12");
 
 F1D func = new F1D("func","[a]+[b]*x+[c]*x*x+[amp]*gaus(x,[mean],[sigma])",.0,1.0);
 func.setParameters(1.0,2.0,3.0,200,0.45,0.2);
@@ -39,14 +39,10 @@ func.show();
 PaveText text = new PaveText("Random Gaussian",0.35,0.2);
 text.addLine("Generated with Java Random");
 
-TGCanvas c = new TGCanvas("myTwig",700,700);
+TGCanvas c = new TGCanvas("myTwig",600,600);
 c.draw(h,"EP").draw(func,"same");
 c.region().showLegend(0.05,0.95);
 c.region().showStats(1.01,1.01);
 c.draw(text);
-/*
-c.cd(0).draw(graph).draw(func,"same");
-c.region(0).showLegend(0.05,0.95);
-c.region(0).showStats(0.60,0.75);
-c.view().export("figure_advanced_graph_fitting.png"); // to save to PDF change the extention to .pdf
-*/
+c.repaint();
+c.view().export("figure_advanced_h1f_fitting.png");
